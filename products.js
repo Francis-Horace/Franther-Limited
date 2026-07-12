cb6d720a93da20d298709cf0b68c367e239c4b6f
Francis-Horace <francisalika7@gmail.com>
2026-07-12 04:01:55 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,51 @@
+const products = [
+
+{
+    id:1,
+    name:"Fried Groundnuts",
+    image:"images/fried-groundnuts.jpg",
+    description:"Freshly fried premium groundnuts.",
+    package:"Available in small, medium and large packs."
+},
+
+{
+    id:2,
+    name:"Raw Groundnuts",
+    image:"images/raw-groundnuts.jpg",
+    description:"High-quality raw groundnuts.",
+    package:"Retail and wholesale."
+},
+
+{
+    id:3,
+    name:"Bulk Groundnuts",
+    image:"images/bulk-groundnuts.jpg",
+    description:"Perfect for businesses and distributors.",
+    package:"50kg Bags."
+},
+
+{
+    id:4,
+    name:"Potato Chips",
+    image:"images/chips.jpg",
+    description:"Crunchy potato chips with delicious flavour.",
+    package:"Available in different sizes."
+},
+
+{
+    id:5,
+    name:"Chin-Chin",
+    image:"images/chin-chin.jpg",
+    description:"Sweet crunchy Chin-Chin made with premium ingredients.",
+    package:"Small, Medium and Family Packs."
+},
+
+{
+    id:6,
+    name:"Honey & Bread",
+    image:"images/honey.jpg",
+    description:"Pure natural honey served with freshly baked bread.",
+    package:"Different honey bottle sizes available."
+}
+
+];
